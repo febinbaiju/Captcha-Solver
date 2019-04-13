4f5e6d7347300569fced6448a19e8372de003667
Febin Baiju <mysticbits2013@gmail.com>
2019-04-13 20:12:15 +0530

    model created

--- model-create.py
+++ model-create.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
+import csv
+import os
 
+fields = ["Digit","X","Y"]
+dataset_file = "dataset.csv"
 for iterate in range(1,10):
     read = cv2.imread("datasets/{0}.png".format(iterate),0)
     mat,converted = cv2.threshold(read,127,255,cv2.THRESH_BINARY)
@@ -16,8 +20,20 @@ for iterate in range(1,10):
     y=0
     for index in range(0,column_values.shape[0]):
         y+=column_values[index]
-    
-    print(x,y)
-    cv2.imshow("image",converted)
-    cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+    field = [iterate,x,y]
+    if iterate<=1:
+        if os.path.exists(dataset_file):
+            os.remove(dataset_file)
+    with open(dataset_file,'a') as fh:
+        writer = csv.writer(fh)
+        if iterate<=1:
+            writer.writerow(fields)
+            print(fields)
+            writer.writerow(field)
+            print(field)
+        else:
+            writer.writerow(field)
+            print(field)
+
+print("Dataset Generated")
