3d91512dcdb45e17283929040fbda0239846620d
Febin Baiju <mysticbits2013@gmail.com>
2019-04-18 17:12:24 +0530

    Byepassed CAPTCHA of AFREESMS.COM

--- KNN.py
+++ KNN.py
@@ -3,80 +3,86 @@ import csv
 import numpy as np
 import math
 
-captcha_identified = []
-dataset_file = "dataset.csv"
-
-filepath = "test.png"
-
-imread = cv2.imread(filepath,0)
-height,width = imread.shape
-roi = imread[0:16,0:width]
-imread=roi 
-#convert to black and white
-mat,bnw = cv2.threshold(imread,127,255,cv2.THRESH_BINARY)
-contours,hierarchy = cv2.findContours(bnw,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-#get individual datasets
-for index in range(1,len(contours)):
-    draw = np.zeros(imread.shape,dtype="uint8")
-    draw.fill(255)
-    cv2.drawContours(draw,contours,index,(0,0,0),1)
-    (x,y,w,h) = cv2.boundingRect(contours[index])
-    cropped = imread[y:y+h,x:x+w]
-    mar2,bnww = cv2.threshold(cropped,127,255,cv2.THRESH_BINARY)
-    cropped = bnww
-        
-    matrix = np.array(cropped)
-    column_values = np.sum(matrix,axis=1)
-    row_values = np.sum(matrix,axis=0)
+def get_captcha(filepath="test.png",demo=False):
+    captcha_identified = []
+    dataset_file = "dataset.csv"
     
-    x=0
-    for index in range(0,row_values.shape[0]):
-        x+=row_values[index]
+    imread = cv2.imread(filepath,0)
+    height,width = imread.shape
+    roi = imread[0:16,0:width]
+    imread=roi 
+    #convert to black and white
+    mat,bnw = cv2.threshold(imread,127,255,cv2.THRESH_BINARY)
+    contours,hierarchy = cv2.findContours(bnw,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #get individual datasets
+    for index in range(1,len(contours)):
+        draw = np.zeros(imread.shape,dtype="uint8")
+        draw.fill(255)
+        cv2.drawContours(draw,contours,index,(0,0,0),1)
+        (x,y,w,h) = cv2.boundingRect(contours[index])
+        cropped = imread[y:y+h,x:x+w]
+        mar2,bnww = cv2.threshold(cropped,127,255,cv2.THRESH_BINARY)
+        cropped = bnww
         
-    y=0
-    for index in range(0,column_values.shape[0]):
-        y+=column_values[index]
-
-    eucs = []
-
-    #read datasets from csv
-    with open(dataset_file,'r') as fh:
-        csvreader = csv.reader(fh)
-        iter=0
-        for row in csvreader:
-            iter+=1
-            if iter<=1:
-                continue
-            row[1] = float(row[1])
-            row[2] = float(row[2])
-
-    #Euclidean distance Formula
-            q1 = (row[1]-x)*(row[1]-x)
-            q2 = (row[2]-y)*(row[2]-y)
-
-            euc = math.sqrt(q1+q2)
-            eucs.append(euc)
+        matrix = np.array(cropped)
+        column_values = np.sum(matrix,axis=1)
+        row_values = np.sum(matrix,axis=0)
     
-    temp_store = eucs.copy()
-    temp_store.sort()
-    min_euc = temp_store[0]
+        x=0
+        for index in range(0,row_values.shape[0]):
+            x+=row_values[index]
+        
+        y=0
+        for index in range(0,column_values.shape[0]):
+            y+=column_values[index]
 
-    if min_euc>x:
-        continue
+        eucs = []
 
-    #finding the index of euclidean distance
-    index = eucs.index(min_euc)+1
-    captcha_identified.append(index)
-    print("identified: "+str(index))
+        #read datasets from csv
+        with open(dataset_file,'r') as fh:
+            csvreader = csv.reader(fh)
+            iter=0
+            for row in csvreader:
+                iter+=1
+                if iter<=1:
+                    continue
+                row[1] = float(row[1])
+                row[2] = float(row[2])
 
-captcha_identified.reverse()
-if len(captcha_identified)>0:
-    print("\n")
-    print("CAPTCHA DECODED: ")
-    print(captcha_identified[:len(captcha_identified)])
-else:
-    print("CAPTCHA DECODING FAILED")        
-cv2.imshow("image",cv2.imread(filepath))
-cv2.waitKey(0)
+            #Euclidean distance Formula
+                q1 = (row[1]-x)*(row[1]-x)
+                q2 = (row[2]-y)*(row[2]-y)
 
-cv2.destroyAllWindows()
+                euc = math.sqrt(q1+q2)
+                eucs.append(euc)
+    
+        temp_store = eucs.copy()
+        temp_store.sort()
+        min_euc = temp_store[0]
+
+        if min_euc>x:
+            continue
+
+        #finding the index of euclidean distance
+        index = eucs.index(min_euc)+1
+        captcha_identified.append(index)
+        print("identified: "+str(index))
+
+    captcha_identified.reverse()
+    
+    if demo==False:
+        decoded_captcha = ''.join(str(e) for e in captcha_identified)
+        return decoded_captcha
+
+    if len(captcha_identified)>0:
+        print("\n")
+        print("CAPTCHA DECODED: ")
+        print(captcha_identified[:len(captcha_identified)])
+    else:
+        print("CAPTCHA DECODING FAILED")        
+    cv2.imshow("image",cv2.imread(filepath))
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+#print(get_captcha("test.png",True))
--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
+from KNN import get_captcha
 import cv2
 
 mobile_no = input("Input Mobile Number: ")
@@ -12,7 +13,7 @@ options.headless = True
 profile = webdriver.FirefoxProfile()
 profile.set_preference("webdriver.load.strategy", "unstable")
 profile.update_preferences()
-obj = webdriver.Firefox(firefox_profile=profile,timeout=5)
+obj = webdriver.Firefox(firefox_profile=profile,timeout=5,options=options)
 obj.get("http://www.afreesms.com/intl/india")
 obj.execute_script("return window.stop")
 
@@ -29,12 +30,17 @@ obj.save_screenshot("image.png")
 captcha = cv2.imread("image.png")
 captcha = captcha[0:28,506:582]
 cv2.imwrite("image.png",captcha)
+captcha_found = get_captcha("image.png")
+if len(captcha_found)<1:
+    print("CAPTCHA FAILED TO IDENTIFY")
+    exit
 
 elem = obj.find_element_by_xpath("//input[2]")
 elem.send_keys(mobile_no)
 textarea = obj.find_element_by_xpath("//textarea[1]")
 textarea.send_keys(message)
-verification.send_keys("124")
+verification.send_keys(captcha_found)
 verification.send_keys(Keys.RETURN)
+
 
 obj.close()
