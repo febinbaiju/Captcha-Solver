bd5b1e050b1d1dd84b41acb48c9ba79b87a764b5
Febin Baiju <mysticbits2013@gmail.com>
2019-04-18 14:13:27 +0530

    mimicked submit send

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
 
 options = Options()
 options.headless = True
@@ -9,4 +10,6 @@ elem = obj.find_element_by_xpath("//input[2]")
 elem.send_keys("9539639217")
 textarea = obj.find_element_by_xpath("//textarea[1]")
 textarea.send_keys("hello world")
-print("hello world")
+verification = obj.find_element_by_xpath("//input[@class='auto']")
+verification.send_keys("124")
+verification.send_keys(Keys.RETURN)
